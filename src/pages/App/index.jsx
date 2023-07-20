60223b53221a7e6aa1575acda53ae7d5f6ebd201
Brayan Monge <bmongemendez@gmail.com>
2023-07-20 02:02:21 -0600

    Feat: Configure initial routes

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,23 +1,51 @@
-import Home from '../Home'
-import MyAccount from '../MyAccount'
-import MyOrder from '../MyOrder'
-import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
-import SignIn from '../SignIn'
+import { useRoutes, BrowserRouter } from "react-router-dom";
 
-import './App.css'
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import SignIn from "../SignIn";
+import NotFound from "../NotFound";
 
-function App() {
-  return (
-      <div className="bg-red-100">
-        <Home/>
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-      </div>
-  )
-}
+import "./App.css";
+
+const AppRoutes = () => {
+	let routes = useRoutes([
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: "/my-account",
+			element: <MyAccount />,
+		},
+		{
+			path: "/my-order",
+			element: <MyOrder />,
+		},
+		{
+			path: "/my-orders",
+			element: <MyOrders />,
+		},
+		{
+			path: "/sign-in",
+			element: <SignIn />,
+		},
+		{
+			path: "/*",
+			element: <NotFound />,
+		},
+	]);
+
+	return routes;
+};
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
+	);
+};
 
 export default App;
