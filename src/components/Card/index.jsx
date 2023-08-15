77819a2550026df8bc857fe4223feefbf7bd95ae
Brayan Monge <bmongemendez@gmail.com>
2023-08-14 23:30:17 -0600

    Fix: Spelling Mistake

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -9,7 +9,7 @@ export default function Card() {
 			<figure className="border-solid border border-black rounded-md relative hover:outline-8 outline-black outline-8 transition-transform box-border hover:shadow-lg hover:translate-x-0.5 hover:-translate-y-0.5 overflow-hidden">
 				<span className=" absolute left-1 top-1 bg-white/60 border py-2 px-3 rounded-md">
 					<TagIcon className="h-6 w-6 text-blue-500 inline mr-1" />
-					Elecronics
+					Electronics
 				</span>
 				<img
 					className="object-center object-cover aspect-square w-full h-full"
