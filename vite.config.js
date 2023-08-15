06dc96c6786ba30ad3e7ce832d0d76641638242f
Brayan Monge <bmongemendez@gmail.com>
2023-08-15 16:32:55 -0600

    Refractor: Fix linting error on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+// Disabled import/no-extraneous-dependencies as Vite automatically
+// creates the projects with these packages as devDependencies
+
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
