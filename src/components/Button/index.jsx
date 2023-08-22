827974fb26acbf77fc784a6cd74f3842224d2b47
Brayan Monge <bmongemendez@gmail.com>
2023-08-22 01:14:25 -0600

    Feat: Add 'purpose' prop to Button Component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Button({ type, text, className, title, onClick }) {
+function Button({ type, text, className, title, onClick, purpose }) {
+	const getButtonPurpose = (purposeType) => {
+		switch (purposeType) {
+			case "primary":
+				return "bg-blue-500 hover:bg-blue-400";
+			case "secondary":
+				return "bg-gray-100 hover:bg-gray-50 text-black";
+			case "error":
+				return "bg-red-500 hover:bg-red-400 text-black";
+			case "success":
+				return "bg-green-500 hover:bg-green-400 text-black";
+			default:
+				return "";
+		}
+	};
+
+	const buttonPurpose = getButtonPurpose(purpose);
+
 	return (
 		<button
 			title={title}
-			className={`text-white border-solid border flex justify-center border-black rounded-md px-4 md:px-5 py-2 md:py-3 bg-blue-500 hover:bg-blue-400 cursor-pointer hover:scale-y-105 transition-transform origin-bottom text-center ${className}`}
+			className={`${className} text-white border-solid border flex bg justify-center items-center border-black rounded-md ${buttonPurpose} bg cursor-pointer hover:scale-y-105 transition-transform origin-bottom text-center`}
 			// eslint-disable-next-line react/button-has-type
 			type={type}
 			onClick={onClick}
@@ -21,12 +38,14 @@ Button.propTypes = {
 	className: PropTypes.string,
 	onClick: PropTypes.func,
 	title: PropTypes.string,
+	purpose: PropTypes.oneOf(["primary", "secondary", "error", "success"]),
 };
 
 Button.defaultProps = {
 	className: "",
 	onClick: () => null,
 	title: "",
+	purpose: "primary",
 };
 
 export default React.memo(Button);
