5d37daebd19d80ff819771696d874b26a7abb82f
Brayan Monge <bmongemendez@gmail.com>
2023-08-22 01:16:44 -0600

    Refractor: Remove unnecessary code on Layout Component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,19 +1,7 @@
-import { useContext } from "react";
 import PropTypes from "prop-types";
-import { StoreContext } from "../../context";
 
 function Layout({ children }) {
-	const { isProductDetailsOpen } = useContext(StoreContext);
-
-	return (
-		<main
-			className={`${
-				isProductDetailsOpen ? "overflow-hidden" : "overflow-auto"
-			} flex mt-20 flex-col items-center`}
-		>
-			{children}
-		</main>
-	);
+	return <main className="flex mt-20 flex-col items-center">{children}</main>;
 }
 
 Layout.propTypes = {
